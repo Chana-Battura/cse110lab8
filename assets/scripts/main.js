6d700ee098f9ffe6e1f8c06a6fd646075cd2cfb5
Charan Battula <charanb2005@gmail.com>
2025-05-27 20:10:29 -0700

    Finished Expose Service Worker

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,14 +45,27 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
+  if ("serviceWorker" in navigator){
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener("load", (event) => {     
+      // Steps B3-B6 will be *inside* the event listener's function created in B2
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      //            "Using Service Workers" will help you here)
+      navigator.serviceWorker.register("/sw.js").then(  
+        // B4. TODO - Once the service worker has been successfully registered, console
+        //            log that it was successful.
+        (registration) => {
+          console.log("Sucessfully registered service worker.  Here is registration status", registration);
+        },
+        (error) => {
+        // B5. TODO - In the event that the service worker registration fails, console
+        //            log that it has failed.
+          console.error(`Failed registering service worker with following error: ${error}`);
+        },
+      );
+    });
+  }
+
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
@@ -70,7 +83,7 @@ async function getRecipes() {
   //            If there are recipes, return them.
   let recipes = localStorage.getItem("recipes");
   if (recipes !== null){
-    return recipes;
+    return JSON.parse(recipes);
   }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
@@ -112,6 +125,7 @@ async function getRecipes() {
     } catch (error){
       // A10. TODO - Log any errors from catch using console.error
       // A11. TODO - Pass any errors to the Promise's reject() function
+      
       console.log(error);
       reject(error);
     }
